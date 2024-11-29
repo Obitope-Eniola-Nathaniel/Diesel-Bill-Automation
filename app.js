e1f22da0912c49896a96bd8ca6922febd104d853
Nathaniel <obitopeeniola@gmail.com>
2024-11-29 06:35:14 +0100

    border fix

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const btn1 = document.getElementById('btn1');
 
-btn1.addEventListener('click', (e) => {
+btn1.addEventListener('submit', (e) => {
     e.preventDefault()
     console.log("Hello Nathaniel")
 })
